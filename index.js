0bbdc5bf6341185e2e572fb8da95b71e6571e715
Sivaram Kumar <sivaramit14@gmail.com>
2019-07-19 14:49:33 +0530

    Changed the time zone

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("Europe/Amsterdam");
+    currentTime.setTimezone("Europe/Copenhagen");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Melbourne is: ' + currentTime.toString(),
+        body: 'The time in Europe/Copenhagen is: ' + currentTime.toString(),
     });
 };
